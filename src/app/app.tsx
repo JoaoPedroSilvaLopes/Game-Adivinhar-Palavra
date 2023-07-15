ca444113bba3a31a6c5ab9ab07cd6afaaf400e53
Joao <finkl1@hotmail.com>
2023-07-15 14:28:25 -0300

    :sparkles: feat: first commit

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,5 @@
 import styled from 'styled-components';
 
-import NxWelcome from './nx-welcome';
-
 const StyledApp = styled.div`
   // Your style here
 `;
@@ -9,7 +7,7 @@ const StyledApp = styled.div`
 export function App() {
   return (
     <StyledApp>
-      <NxWelcome title="adivinhar-palavra" />
+      <div>adivinhar-palavra</div>
     </StyledApp>
   );
 }
